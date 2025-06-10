10632de46dccafcd3d6d3dceeb436c99bc7a40ce
jusbraun <jusbraun>
2025-06-10 12:19:50 -0700

    clean up

--- test_model.py
+++
@@ -1,9 +0,0 @@
-import pickle 
-import joblib as jl
-import xgboost
-import warnings
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-warnings.filterwarnings("ignore", category=FutureWarning)
-
